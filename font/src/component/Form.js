be94b2613ebeed7c40e26ede3f916858133675eb
alongkot2538 <alongkot2538>
2020-03-24 01:33:43 +0700

    second

--- font/src/component/Form.js
+++ font/src/component/Form.js
@@ -1,19 +1,35 @@
 import  React from 'react'
+import {useSelector,useDispatch} from 'react-redux'
+import axios from 'axios'
 
 const Form =()=>{
+    const dispatch = useDispatch();
+    const form = useSelector(state => state.form)
+    const books = useSelector(state => state.book)
+
+   
+    const addBook = async () => {
+       await axios.post(`http://localhost/api/students`, form)
+        dispatch({
+            type: 'ADD_BOOK', book: {
+                id: books.length > 0 ? books[books.length-1].no+1 : 0,
+                ...form
+            }
+        })
+    }
 return(
     <div>
        <input
-             type="text"
+             type="name"
              placeholder="Enter name" 
-            //  onChange={(e) => dispatch({ type: 'CHANGE_NAME', name: e.target.value })}
+             onChange={(e) => dispatch({ type: 'CHANGE_NAME', name: e.target.value })}
             />
             <input
-             type="text"
-             placeholder="Enter surname" 
+             type="number"
+             placeholder="Enter cost" 
             //  onChange={(e) => dispatch({ type: 'CHANGE_SURNAME', surname: e.target.value })}
             />
-            <input
+            {/* <input
              type="text"
              placeholder="Enter id" 
             //  onChange={(e) => dispatch({ type: 'CHANGE_ID', id: e.target.value })}
@@ -27,8 +43,8 @@ return(
              type="number"
              placeholder="Enter GPA" 
             //  onChange={(e) => dispatch({ type: 'CHANGE_GPA', GPA: e.target.value })}
-            />
-            <button>ADD</button>
+            /> */}
+            <button onClick ={addBook }>ADD</button>
     </div>
 )
 }
